b634029128f6978a1c7a064324ab64b3d1969957
vii pieri <128058367+upvii@users.noreply.github.com>
2024-04-29 11:46:47 -0300

    Add files via upload

--- meus objetivos/main.js
+++ meus objetivos/main.js
@@ -1,5 +1,8 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-11-05T00:00:00");
+const tempoAtual = new Date;
 //Declara quais elementos vão ser lidos. O script vai ler todos que tiverem a classe "botao".
 
 for(let i=0;i <botoes.length;i++){
@@ -15,3 +18,5 @@ for(let i=0;i <botoes.length;i++){
         //Adiciona a classe ativo só no botão clicado.
     }
 }
+
+contadores[0].textContent=tempoObjetivo1-tempoAtual;
