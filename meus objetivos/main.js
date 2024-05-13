508292e462133378a6dbfc28e6b42216bd59da78
vii pieri <128058367+upvii@users.noreply.github.com>
2024-05-13 10:20:41 -0300

    Add files via upload

--- meus objetivos/main.js
+++ meus objetivos/main.js
@@ -24,11 +24,7 @@ for(let i=0;i <botoes.length;i++){
     }
 }
 
-contadores[0].textContent=calculaTempo(tempoObjetivo1);
-contadores[1].textContent=calculaTempo(tempoObjetivo2);
-contadores[2].textContent=calculaTempo(tempoObjetivo3);
-contadores[3].textContent=calculaTempo(tempoObjetivo4);
-contadores[4].textContent=calculaTempo(tempoObjetivo5);
+
 
 
 function calculaTempo(tempoObjetivo){
@@ -45,3 +41,18 @@ function calculaTempo(tempoObjetivo){
 
     return dias+" dias "+horas+" horas "+minutos+" minutos "+segundos+" segundos ";
 }
+
+comecaCronometro();
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000);
+}
+
+    function atualizaCronometro(){
+        contadores[0].textContent=calculaTempo(tempoObjetivo1);
+        contadores[1].textContent=calculaTempo(tempoObjetivo2);
+        contadores[2].textContent=calculaTempo(tempoObjetivo3);
+        contadores[3].textContent=calculaTempo(tempoObjetivo4);
+        contadores[4].textContent=calculaTempo(tempoObjetivo5);
+    }
